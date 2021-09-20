360382d324bbdb265a16cb167fe03d499551221f
adonai-luque <akluqueprieto@gmail.com>
2021-09-20 02:49:42 -0500

    Company model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_074329) do
+ActiveRecord::Schema.define(version: 2021_09_20_074919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "start_date"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "critics", force: :cascade do |t|
     t.string "title"
