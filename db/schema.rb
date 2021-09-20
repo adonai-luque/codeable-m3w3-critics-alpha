8845686499df6a71f762efdde83846c3d69c16c5
adonai-luque <akluqueprieto@gmail.com>
2021-09-20 02:47:38 -0500

    Critic model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_220350) do
+ActiveRecord::Schema.define(version: 2021_09_20_074329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "critics", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_critics_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -26,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_09_19_220350) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "critics", "users"
 end
